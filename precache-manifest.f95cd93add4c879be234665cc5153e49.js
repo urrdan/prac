8a66e1a81d664b28856875105492311d397b76eb
urrdan <yarae99@gmail.com>
2019-04-27 18:44:18 -0500

    Updates

---
+++ precache-manifest.f95cd93add4c879be234665cc5153e49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88a7350be6a844ea36e42cb69faf77c",
+    "url": "/prac/index.html"
+  },
+  {
+    "revision": "1c241590c511bb0de793",
+    "url": "/prac/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/prac/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1c241590c511bb0de793",
+    "url": "/prac/static/js/main.0892e00e.chunk.js"
+  },
+  {
+    "revision": "4936be726c2b7a69b7a8",
+    "url": "/prac/static/js/runtime~main.9abf6fff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/prac/static/media/logo.5d5d9eef.svg"
+  }
+]);
